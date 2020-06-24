8ecbaba5f69ece8b259d783add02cff2c7c538dc
Lo <serigne.lo@trimedx.com>
2020-06-24 10:38:25 -0400

    Add orders to server

--- store/actions/orders.js
+++ store/actions/orders.js
@@ -2,7 +2,40 @@ export const ADD_ORDER = 'ADD_ORDER';
 
 
 export const addOrder = (cartItem, totalAmount) => {
-    return {
-        type: ADD_ORDER, 
-        orderData: {items: cartItem, amount: totalAmount}}
+    return async dispatch => {
+        const date = new Date();
+        //execute any async code we want
+        const response = await fetch('https://rn-complete-guide-3cfd6.firebaseio.com/orders/u1.json', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                cartItem, 
+                totalAmount,
+                date: date.toISOString()
+            })
+        })
+
+        if(!response.ok){
+            throw new Error('Something went wrong!');
+        }
+
+        const resData = await response.json();
+
+        dispatch({
+            type: ADD_ORDER,
+            orderData: { 
+                id: resData.name,
+                items: cartItem, 
+                amount: totalAmount,
+                date: date
+            }
+        });
+    }
+
+    // return {
+    //     type: ADD_ORDER,
+    //     orderData: { items: cartItem, amount: totalAmount }
+    // }
 }
